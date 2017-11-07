b37a67d7df1487f9fe6783449ec3da59b03f6b08
jani-123 <janina.rqa@gmail.com>
2017-11-07 17:01:31 -0500

    subido cambio

--- src/store.js
+++ src/store.js
@@ -1,5 +1,17 @@
 import createStore from "redux-zero";
 
+const LISTCOMMENT = [
+	{
+		 id: 0,
+		 title:'Tarea hackaton',
+		 comment: 'me gusta la pagina'
+	},
+	{
+	 id: 1,
+	 name:'Maricielo',
+	 comment: 'Esta pagina es super!!!'
+}
+];
 const initialState = {
 	active: false,
 };
